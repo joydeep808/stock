f6dd9eca757fdd863b83f2d8af7cdf4a64786271
Joydeep Debnath <joydeepdebnath868@gmail.com>
2025-01-02 15:24:11 +0530

    initial

---
+++ src/main/java/com/jstock/jstock/JstockApplication.java
@@ -0,0 +1,13 @@
+package com.jstock.jstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jstock/jstock/JstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jstock.jstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
